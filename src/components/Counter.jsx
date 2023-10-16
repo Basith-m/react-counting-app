2aca4270c095b002e0cac08634523bf571054a95
Basith-m <abm2k20@gmail.com>
2023-10-16 12:39:32 +0530

    counting v2

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -22,13 +22,13 @@ const dispatch = useDispatch()
     <div style={{height:'70vh'}} className='w-100 d-flex justify-content-center align-items-center mt-5'>
         <div className='d-flex justify-content-between align-items-center border rounded p-2 w-25 flex-column'>
             <h1 style={{fontSize:'100px'}}>{count}</h1>
-            <div className='d-flex justify-content-between align-items-center w-100 mt-5'>
+            <div className='d-flex justify-content-around align-items-center w-100 mt-5'>
                 <button className='btn btn-warning' onClick={()=>dispatch(decrement())}>Decrement</button>
                 <button className='btn btn-danger' onClick={()=>dispatch(reset())}>Reset</button>
                 <button className='btn btn-success' onClick={()=>dispatch(increment())}>Increment</button>
             </div>
-            <div className='d-flex mt-5 mb-3'>
-              <input type="text" placeholder='Enter the amout to be increment' className='form-control' onChange={(e)=>setAmount(e.target.value)} /> 
+            <div className='d-flex mt-5 mb-3 w-100'>
+              <input style={{fontSize:'15px'}} type="text" placeholder='Enter the amout to be increment' className='form-control' onChange={(e)=>setAmount(e.target.value)} /> 
               <button className='btn btn-primary ms-3' onClick={handleIncrement}>Increment by</button>
             </div>
         </div>
